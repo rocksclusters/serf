029e0c401e48c60224178527010ff23da46e43a1
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-09-11 13:46:03 -0700

    serf for 7

--- src/serf/version.mk
+++ src/serf/version.mk
@@ -1,3 +1,25 @@
 PKGROOT	= /opt/rocks/bin
 NAME	= serf
 RELEASE = 0
+
+INITD_DIR	= /etc/init.d
+INITD_SCRIPTS	= rocks-serf
+SYSTEMD_DIR	= /etc/systemd/system
+SYSTEMD_SCRIPTS = rocks-serf.service
+
+ifeq ($(VERSION.MAJOR),6)
+SCRIPTDIR = $(INITD_DIR)
+SCRIPTS = $(INITD_SCRIPTS)
+endif
+ifeq ($(VERSION.MAJOR),7)
+SCRIPTDIR = $(SYSTEMD_DIR)
+SCRIPTS = $(SYSTEMD_SCRIPTS)
+RPM.SCRIPTLETS.FILE = scriptlets
+endif
+
+RPM.FILES	= \
+$(PKGROOT)/*\n\
+/var/opt/rocks/$(NAME)\n\
+/etc/$(NAME)\n\
+/etc/logrotate.d/*\n\
+$(SCRIPTDIR)
